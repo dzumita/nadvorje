04f6bcedd2dee12c6f1d1e99940fd9e2478b9786
dzumita <60666903+dzumita@users.noreply.github.com>
2022-03-04 20:56:04 +0300

    add pl lang

--- translations/index.ts
+++ translations/index.ts
@@ -2,12 +2,14 @@ import en from './en';
 import ru from './ru';
 import by from './by';
 import ua from './ua';
+import pl from './pl';
 
 const translations = {
   en,
   ru,
   by,
   ua,
+  pl,
 };
 
 export default translations;
---
+++ translations/pl.ts
@@ -0,0 +1,25 @@
+const pl = {
+  error: 'Nie udało się wczytać pogody, spróbuj w innym mieście.',
+  searchPlaceholder: 'Wpisz miasto w języku angielskim',
+  weather: {
+    sn: '🌨️ Śnieg 🌨️',
+    sl: '🌧️ Deszcz Ze Śniegiem 🌧️',
+    h: '🌧️ Grad 🌧️',
+    t: '🌩️ Burza Z Piorunami 🌩️',
+    hr: '🌧️ Ulewa 🌧️',
+    lr: '🌧️ Lekki Deszcz 🌧️',
+    s: '🌧️ Prysznice 🌧️',
+    hc: '☁️ Ciężka Chmura ☁️',
+    lc: '🌤️ Lekka Chmura 🌤️',
+    c: '☀️ Ładna Pogoda ☀️',
+  },
+  navigator: {
+    weather: 'Pogoda',
+    settings: 'Ustawienia',
+  },
+  settings: {
+    language: 'Język:',
+  },
+};
+
+export default pl;
