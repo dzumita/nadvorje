45363a83791021d94f2d94135a2ac161bacd24ed
dzumita <60666903+dzumita@users.noreply.github.com>
2022-03-04 20:48:06 +0300

    add ua lang

--- translations/index.ts
+++ translations/index.ts
@@ -1,11 +1,13 @@
 import en from './en';
 import ru from './ru';
 import by from './by';
+import ua from './ua';
 
 const translations = {
   en,
   ru,
   by,
+  ua,
 };
 
 export default translations;
